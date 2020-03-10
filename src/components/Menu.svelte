2c70094e86eec00a850267aac6a611491e851502
OlivierBelaud <olivierbelaudpro@gmail.com>
2020-03-10 16:33:41 +0100

    merge port

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,11 +1,4 @@
 <script>
-let menuOpen = false
-
-const OpenMenu = function() {
-  console.log('OpenMenu')
-  menuOpen = true
-}
-
 </script>
 
 <div class="relative bg-white overflow-hidden">
@@ -16,81 +9,23 @@ const OpenMenu = function() {
           <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
             <div class="flex items-center justify-between w-full md:w-auto">
               <a href="/">
-                <img class="h-6 w-auto sm:h-6" src="https://cohhort.com/_nuxt/img/c9f4d01.png" alt="" />
+                <!-- <img class="h-6 w-auto sm:h-6" src="https://cohhort.com/_nuxt/img/c9f4d01.png" alt="" /> -->
               </a>
-              <!-- <div class="-mr-2 flex items-center md:hidden">
-                <button on-click={OpenMenu()} type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                  <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                  </svg>
-                </button>
-              </div> -->
             </div>
           </div>
-          <!-- <div class="hidden md:block md:ml-10 md:pr-4">
-            <a href="/" class="font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out">Product</a>
-            <a href="/" class="ml-8 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out">Features</a>
-            <a href="/" class="ml-8 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out">Marketplace</a>
-            <a href="/" class="ml-8 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out">Company</a>
-            <a href="/" class="ml-8 font-medium text-indigo-600 hover:text-indigo-900 focus:outline-none focus:text-indigo-700 transition duration-150 ease-in-out">Log in</a>
-          </div> -->
         </nav>
       </div>
-
-      {#if menuOpen}
-        <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-          <div class="rounded-lg shadow-md">
-            <div class="rounded-lg bg-white shadow-xs overflow-hidden">
-              <div class="px-5 pt-4 flex items-center justify-between">
-                <div>
-                  <!-- <img class="h-8 w-auto" src="/img/logos/workflow-mark-on-white.svg" alt="" /> -->
-                </div>
-                <div class="-mr-2">
-                  <button on-click="{menuOpen = false}" type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-              <!-- <div class="px-2 pt-2 pb-3">
-                <a href="/" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">Product</a>
-                <a href="/" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">Features</a>
-                <a href="/" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">Marketplace</a>
-                <a href="/" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">Company</a>
-              </div> -->
-              <!-- <div>
-                <a href="/" class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100 hover:text-indigo-700 focus:outline-none focus:bg-gray-100 focus:text-indigo-700 transition duration-150 ease-in-out">
-                  Log in
-                </a>
-              </div> -->
-            </div>
-          </div>
-        </div>
-      {/if}
 
       <div class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
         <div class="sm:text-center lg:text-left">
           <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
             Real Time Graphics to better understand the dynamic of the
             <br class="xl:hidden" />
-            <span class="text-indigo-600">Covid19 epidemie</span>
+            <span class="text-cored-600">Covid19 epidemie</span>
           </h2>
           <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
           </p>
-          <!-- <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-            <div class="rounded-md shadow">
-              <a href="/" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
-                Get started
-              </a>
-            </div>
-            <div class="mt-3 sm:mt-0 sm:ml-3">
-              <a href="/" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
-                Live demo
-              </a>
-            </div>
-          </div> -->
         </div>
       </div>
       <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none">
@@ -99,6 +34,6 @@ const OpenMenu = function() {
     </div>
   </div>
   <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-    <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://www.state.gov/wp-content/uploads/2020/03/2871-2560x852.png" alt="" />
+    <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1583324113626-70df0f4deaab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2978&q=80" alt="" />
   </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        coblue: '#ff4f81',
+        cored: '#ff6c5f',
+        coorange: '#ffc168',
+        coyellow: '#2dde98',
+        cogreen: '#1cc7d0',
+        coazul: '#00aeff',
+        copurple: '#8e43e7',
+      }
+    },
   },
   variants: {},
-  plugins: [
-    require('tailwindcss-font-inter')()
-  ],
+  plugins: [],
 }
