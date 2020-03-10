6d9facce8a18e5a39c54330fbdff8e743a8efadf
OlivierBelaud <olivierbelaudpro@gmail.com>
2020-03-10 18:34:28 +0100

    font

--- src/App.svelte
+++ src/App.svelte
@@ -9,6 +9,6 @@
 
 <main>
 	<Menu />
-	<!-- <Title title="Covid19" /> -->
-	<!-- <Chart /> -->
+	<Title title="Covid19" />
+	<Chart />
 </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      display: ['Domine', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif'],
+      body: ['Montserrat', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+    },
     extend: {
       colors: {
         coblue: '#ff4f81',
@@ -9,6 +13,9 @@ module.exports = {
         cogreen: '#1cc7d0',
         coazul: '#00aeff',
         copurple: '#8e43e7',
+      },
+      fontSize: {
+        '52xl': '3.5rem',
       }
     },
   },
